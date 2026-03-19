fc1789c4b1a1b7b0e30594b14a6697f6a6091726
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-03-19 07:05:53 +0630

    Update GitHub Actions workflow with real Bedrock TS types

---
+++ types/minecraft.d.ts
@@ -0,0 +1,8 @@
+// types/minecraft.d.ts
+declare module "@minecraft/server" {
+    export * from "bc-minecraft-bedrock-types";
+}
+
+declare module "@minecraft/server-ui" {
+    export * from "bc-minecraft-bedrock-types/ui";
+}
